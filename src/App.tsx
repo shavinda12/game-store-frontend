4cad8e7ae9644286b01e7223b7fd759c85a6d66b
DESKTOP-PJL516Q <nethupama1234@gmail.com>
2024-09-06 19:23:11 +0530

    selecting Games according to genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
-import React from "react";
 import NavBar from "./components/NavBar";
 import GamesGrid from "./components/GamesGrid";
+import GenreList from "./components/GenreList";
+import { useState } from "react";
+import { Genres } from "./hooks/useGenres";
 
 const App = () => {
+  const [selectedGenre, setSelectedGenre] = useState<Genres | null>(null);
   return (
-    <Grid templateAreas={{
-      base:`"nav" "main"`,
-      lg:`"nav nav" "aside main"`
-    }} p={4}>
-      <GridItem area="nav"> <NavBar/> </GridItem>
-      <Show above='lg'>
-      <GridItem area="aside"> Aside </GridItem>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
+      p={4}
+    >
+      <GridItem area="nav">
+        {" "}
+        <NavBar />{" "}
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" paddingX={5}>
+          {" "}
+          <GenreList onSelect={(genre)=>setSelectedGenre(genre)}/>{" "}
+        </GridItem>
       </Show>
-      <GridItem area="main"> <GamesGrid/> </GridItem>
+      <GridItem area="main">
+        {" "}
+        <GamesGrid selectedGenre={selectedGenre}/>{" "}
+      </GridItem>
     </Grid>
   );
 };
---
+++ src/components/LoadingSkeletonSideBar.tsx
@@ -0,0 +1,14 @@
+import { Box, HStack, Skeleton, SkeletonText } from "@chakra-ui/react";
+
+const LoadingSkeletonSideBar = () => {
+  return (
+    <Box>
+      <HStack paddingX={1} paddingY={1}>
+        <Skeleton height="32px" width="30%"/>
+        <Skeleton height="32px" width="40%"/>
+      </HStack>
+    </Box>
+  );
+};
+
+export default LoadingSkeletonSideBar;
